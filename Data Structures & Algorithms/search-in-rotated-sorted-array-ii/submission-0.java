1687d854bf1c67e1eb840a37408ca5c0b20467fb
Sujeth12 <sujeth20055002@gmail.com>
2026-08-04 21:41:01 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0 ; i<nums.length ;i++){
+            set.add(nums[i]);
+
+            if(set.contains(target)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
