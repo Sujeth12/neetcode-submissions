f204058e59bf6fb4d9a033490a92887879a19c04
Sujeth12 <sujeth20055002@gmail.com>
2026-08-09 22:15:15 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer,Integer>  map = new HashMap<>();
+        for(int i =0 ; i < nums . length ; i++){
+            int count = map.getOrDefault(nums[i] , 0)+1;
+            map.put(nums[i] , count);
+            if(count > nums.length /2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
